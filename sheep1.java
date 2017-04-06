45ccd07a6a583038d5fb52b991251465762d81d7
keval ramoliya <kevalramoliya13@gmail.com>
2017-04-06 12:05:13 +0530

    Update sheep1.java

--- sheep1.java
+++ sheep1.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -15,83 +16,66 @@ public class sheep1
     public static ArrayList<String> sNumbers = new ArrayList<String>();
     public static ArrayList<Integer> digits =new ArrayList<Integer>();
     public static Set<Integer> set=new HashSet<Integer>();
-    public static Scanner sc=new Scanner(System.in);
-    public static void main(String args[])
+
+    public static void main(String args[]) throws IOException
     {
-
+         Scanner sc=new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("D:\\codes-master\\codes-master\\src\\com\\company\\A-small.in"))));
         T=sc.nextInt();
-        ArrayList<Integer> numbers=new ArrayList<Integer>(input(T));
-        //System.out.println(numbers);
-        ntoc(numbers);
-        //System.out.println(sNumbers);
-        //cal(numbers);
-
+        //System.out.println(T);
+        ArrayList<Integer> numbers=new ArrayList<Integer>();
+        int arr[]=new int[T];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+            inputnum.add(arr[i]);
+        }
+        ntoc(inputnum);
         int a[]=new int[sNumbers.size()];
         for(int i =0;i<sNumbers.size();i++)
         {
             a[i] = Integer.parseInt(sNumbers.get(i));
-           // System.out.println(a[i]);
+            //System.out.println(a[i]);
         }
         cal(a);
-
     }
-
-    private static void cal(int[] a)
+    public static void cal(int[] a) throws IOException
     {
-
-     for (int i=0;i<a.length;i++)
-        {
-         for (int j=1;j<=200;j++)
+        String output = "";
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\codes-master\\codes-master\\src\\com\\company\\A-small.out")));
+        for (int i=0;i<a.length;i++) {
+            int count=0;
+            if(a[i]==0)
             {
+                output += "Case #" + (i+1) + ": INSOMNIA\n";
+                continue;
+            }
+            for (int j = 1; j <= 200; j++) {
                 int temp = a[i] * j;
-                System.out.println(temp);
-                while (temp > 0)
-                {
+                while (temp > 0) {
                     int temp1;
-                    temp1=  temp % 10;
+                    temp1 = temp % 10;
                     set.add(temp1);
-
                     temp = temp / 10;
-
-
                 }
-                if(set.size()==10)
-                {
-                   System.out.println("temp= "+a[i]*j);
+                if (set.size() == 10) {
+
+                    output += "Case #" + (i+1) + ": " + a[i]*j + "\n";
                     break;
                 }
-
-//System.out.println(a[i]*j);
             }
-            System.out.println(set);
-         set.clear();
+            set.clear();
         }
-
+        bw.write(output);
+        bw.close();
     }
-
-    public static ArrayList<Integer> input(int n)
-    {
-        int arr[]=new int[n];
-       for(int i=0;i<arr.length;i++)
-       {
-           arr[i]=sc.nextInt();
-           inputnum.add(arr[i]);
-       }
-       return inputnum;
-    }
-
-
-
     public static ArrayList<String> ntoc( ArrayList<Integer> num)
     {
 
-        for (Integer i: num) {
+        for (int i: num) {
             sNumbers.add(String.valueOf(i));
         }
         //System.out.println(sNumbers);
         return sNumbers;
     }
-
-
-
 }
+
