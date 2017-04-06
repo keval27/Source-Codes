4760a0a83435a77598cf7d563c7976a1641424d2
keval ramoliya <kevalramoliya13@gmail.com>
2017-04-06 12:32:09 +0530

    Update pancake1.java

--- pancake1.java
+++ pancake1.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,41 +13,31 @@ public class pancake1 {
    public static Scanner sc=new Scanner(System.in);
    public static  ArrayList<String> inputs =new ArrayList<String>();
 
-    public static void main(String args[])
+    public static void main(String args[])throws IOException
     {
+        Scanner sc=new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream("D:\\codes-master\\codes-master\\src\\com\\company\\A-small.in"))));
         T=sc.nextInt();
-        generatestr(T);
-        System.out.println(inputs);
+        for(int i=0;i<T;i++)
+        {
+            inputs.add(sc.next());
+        }
+
+       // System.out.println(inputs);
+        String output = "";
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\codes-master\\codes-master\\src\\com\\company\\A-small.out")));
 
         for(int i=0;i<inputs.size();i++)
         {
-
             char[] ch=inputs.get(i).toCharArray();
-
-
-
-                int count=0;
-                for (int i1 = 1; i1 < ch.length; i1++) {
+            int count=0;
+                for (int i1 = 1; i1<ch.length; i1++)
+                {
                     if (ch[i1] != ch[i1-1]) count++;
                 }
                 if (ch[ch.length-1] == '-') count++;
-
-            System.out.println(inputs.get(i)+" "+count);
-
-        }
-
-
-    }
-
-
-
-    public static ArrayList<String> generatestr(int T)
-    {
-
-        for(int i=0;i<T;i++)
-        {
-          inputs.add(sc.next());
-        }
-        return inputs;
+            output += "Case #" + (i+1) + ": " + count + "\n";
+           // System.out.println(inputs.get(i)+" "+count);
+        } bw.write(output);
+        bw.close();
     }
 }
